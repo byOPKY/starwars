c70f14b3d5b3138ff2aed42aa23a3b36ed2fb0ee
byOPKY <orrillokarol@gmail.com>
2016-11-11 11:05:28 -0500

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+ var template ='<option value="{{number}}">{{especie}}</option>';
+ var plantilla ='<div id="show">{{personas}}</div>'
+			    
+ $(document).ready(function() {
+   
+    $.getJSON("http://swapi.co/api/species/", function(response){
+    	var especies="";
+    	var personajes="";
+    	$.each(response.results, function(i, especie){
+    		var numero ="";
+    		var slach;
+    		for (var i = 0; i < especie.people.length; i++) {
+    			numero += especie.people[i].substr(-3);
+    			slach = numero.substring(0,numero.length-1)
+    		}
+    		especies += template.replace("{{especie}}", especie.name).replace("{{number}}", slach);
+    	}); 
+    	$("#especimen").change(function(event){
+    	    var contador = $(this).val().split("/");
+    		for (var i = 0; i < contador.length; i++) {
+    			$.getJSON("http://swapi.co/api/people/" + contador[i], function(responsep){
+    				personajes += plantilla.replace("{{personas}}", responsep.name);
+    				$("#show").append(personajes);
+    				
+    			});
+    		}
+    	});
+
+    	$("#especimen").append(especies);
+    });
+});
